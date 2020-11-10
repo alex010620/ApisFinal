bc3bcc60f62f8c6ac97f634c2d2a85f21e6accf1
Alex carrera <elalfacarrera@gmail.com>
2020-11-10 17:58:50 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,184 @@
+import sqlite3
+from fastapi import FastAPI
+
+app= FastAPI()
+
+
+@app.get("/")
+def root():
+    return {'Sistema':'Fast Hospital'}
+
+
+#Inicio de session Solo se mostraran los pacientes registrados por el doctor que los registro...
+
+@app.get("/api/iniciar/{correo}/{clave}")
+def create_tuple(correo:str,clave:str):
+    my_list = []
+    Email=""
+    Passw=""
+    conexion = sqlite3.connect("Fasth_Hospital1.s3db")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT idDoctor, Email, Clave FROM Doctores WHERE Email = '"+correo+"' and Clave = '"+clave+"'")
+    contenido1 = cursor.fetchall()
+    conexion.commit()
+    for a in contenido1:
+        idDoctor=a[0]
+        Email=a[1]
+        Passw = a[2]
+    if correo== Email and clave == Passw:
+        cursor.execute("SELECT Cedula, Nombre, Apellido, Tipo_Sangre, Email, Sexo, Fecha_Nacimiento, Alergias, Foto,Zodiaco FROM Pacientes WHERE idDoctor = "+str(idDoctor)+" ")
+        contenido = cursor.fetchall()
+        conexion.commit()
+        for i in contenido:
+            my_list.append({'doctor':idDoctor, 'CedulaPaciente':i[0],'NombrePaciente':i[1],'ApellidoPaciente':i[2],'Tipo_SangrePaciente':i[3],'EmailPaciente':i[4],'SexoPaciente':i[5],'Fecha_NacimientoPaciente':i[6],'AlergiasPaciente':i[7], 'Foto':i[8], 'Zodiaco':i[9]})
+        return my_list
+    else:
+        return {'Ok':False}
+
+#Consulta para Visitas por fecha, podrá seleccionar una fecha y saldrá los pacientes que visitaron en esa fecha.
+
+@app.get("/api/fecha/{fecha}")
+def fecha(fecha:str):
+    datos=[]
+    conexion = sqlite3.connect("Fasth_Hospital1.s3db")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT Paciente,Fecha,Motivo_Consulta,Numero_Seguro,Monto_Pagado,Diagnostico,Nota FROM Consulta WHERE Fecha = '"+fecha+"'")
+    contenido = cursor.fetchall()
+    conexion.commit()
+    for i in contenido:
+        datos.append({"Paciente":i[0],"Fecha":i[1],"Motivo_Consulta":i[2],"Numero_Seguro":i[3],"Monto_Pagado":i[4],"Diagnostico":i[5],"Nota":i[6]})
+    return datos
+
+#Reporte de pacientes con cantidad de visitas, aparecerá un listado con todos los pacientes registrados y al lado la cantidad de consultas que ha hecho ese paciente.
+
+@app.get("/api/idDoctor/{idDoctor}")
+def ConsultaCantidad(idDoctor:str):
+    datos=[]
+    conexion = sqlite3.connect("Fasth_Hospital1.s3db")
+    cursor = conexion.cursor()
+    sql="SELECT D.idDoctor, p.Cedula, p.Nombre, p.Apellido,p.Email,count(c.idPaciente) As Cantidad FROM Pacientes as p INNER JOIN Consulta as c on p.idPaciente = c.idPaciente INNER JOIN Doctores as D on D.idDoctor = c.idDoctor WHERE c.idDoctor = "+idDoctor+" GROUP by c.Paciente"
+    cursor.execute(sql)
+    contenido = cursor.fetchall()
+    conexion.commit()
+    for i in contenido:
+        datos.append({"Cedula":i[1],"Nombre":i[2],"Apellido":i[3],"Email":i[4],"Cantidad":i[5]})
+    return datos
+
+#Reporte zodiacal, aparecerá un listado de todos los pacientes cedula, nombre, apellido y signo zodiacal.
+
+@app.get("/api/zodiaco/{idDoctor}")
+def SignoZodiacal(idDoctor:str):
+    datos=[]
+    conexion = sqlite3.connect("Fasth_Hospital1.s3db")
+    cursor = conexion.cursor()
+    sql="SELECT Cedula, Nombre, Apellido, Zodiaco FROM Pacientes WHERE idDoctor = "+idDoctor+""
+    cursor.execute(sql)
+    contenido = cursor.fetchall()
+    conexion.commit()
+    for i in contenido:
+        datos.append({"Cedula":i[0],"Nombre":i[1],"Apellido":i[2],"Zodiaco":i[3]})
+    return datos
+
+#Registro de Doctores
+
+@app.get("/api/crear/{nombre}/{correo}/{clave}")
+def crear(nombre:str,correo:str,clave:str):
+    try:
+        conexion=sqlite3.connect('Fasth_Hospital1.s3db')
+        registro=conexion.cursor()
+        usuario=(nombre,correo,clave)
+        sql='''INSERT INTO Doctores(Nombre,Email,Clave)VALUES(?,?,?)'''
+        registro.execute(sql,usuario)
+        conexion.commit()
+        return {"respuesta":"Los datos fueros registrados exitosamente"}
+    except TypeError:
+        return {"respuesta":"No se pudieron registrar los datos"}
+
+#Registro de consulta        
+
+@app.get("/api/Consulta/{idPaciente}/{idDoctor}/{Paciente}/{Fecha}/{Motivo_Consulta}/{Numero_Seguro}/{Monto_Pagado}/{Diagnostico}/{Nota}/{Foto_Evidencia}")
+def Consulta(idPaciente: str, idDoctor: str, Paciente: str, Fecha: str, Motivo_Consulta: str, Numero_Seguro: int, Monto_Pagado: int, Diagnostico:str, Nota:str,Foto_Evidencia:str):
+    try:
+        conexion = sqlite3.connect('Fasth_Hospital1.s3db')
+        cursor = conexion.cursor()
+        usuario=(idPaciente,idDoctor,Paciente,Fecha,Motivo_Consulta,Numero_Seguro,Monto_Pagado,Diagnostico,Nota,Foto_Evidencia)
+        sql='''INSERT INTO Consulta(idPaciente,idDoctor,Paciente,Fecha,Motivo_Consulta,Numero_Seguro,Monto_Pagado,Diagnostico,Nota,Foto_Evidencia)VALUES(?,?,?,?,?,?,?,?,?,?)'''
+        cursor.execute(sql,usuario)
+        conexion.commit()
+        return {"respuesta":"Los datos fueros registrados exitosamente"}
+    except:
+        return {"respuesta":"No se pudieron registrar los datos"}
+
+#Actualizar Consulta Paciente
+
+@app.get("/api/ActualizarConsulta/{idConsulta}/{Paciente}/{Fecha}/{Motivo_Consulta}/{Numero_Seguro}/{Diagnostico}/{Monto_Pagado}/{Nota}/{Foto_Evidencia}")
+def ActualizarConsulta(idConsulta:str, Paciente: str, Fecha: str, Motivo_Consulta: str, Numero_Seguro: str,Monto_Pagado:str, Diagnostico:str, Nota:str,Foto_Evidencia:str):
+    try:
+        conexion = sqlite3.connect('Fasth_Hospital1.s3db')
+        cursor = conexion.cursor()
+        sql2="UPDATE Consulta SET Paciente='"+Paciente+"',Fecha='"+Fecha+"',Motivo_Consulta='"+Motivo_Consulta+"',Monto_Pagado='"+Monto_Pagado+"',Numero_Seguro='"+Numero_Seguro+"',Diagnostico='"+Diagnostico+"',Nota='"+Nota+"',Foto_Evidencia='"+Foto_Evidencia+"' WHERE idConsulta = '"+idConsulta+"'"
+        cursor.execute(sql2)
+        conexion.commit()
+        return {"respuesta":"Los datos fueros registrados exitosamente"}
+    except EnvironmentError:
+        return {"respuesta":"No se pudieron actualizar los datos"}
+
+'''
+Registro de pacientes
+'''
+
+@app.get("/api/Pacientes/{idDoctor}/{Cedula}/{Foto}/{Nombre}/{Apellido}/{Tipo_Sangre}/{Email}/{Sexo}/{Fecha_Nacimiento}/{Alergias}/{Zodiaco}")
+def crearPaciente(idDoctor:str,Cedula:str,Foto:str,Nombre:str,Apellido:str,Tipo_Sangre:str,Email:str,Sexo:str,Fecha_Nacimiento:str,Alergias:str,Zodiaco:str):
+    try:
+        conexion=sqlite3.connect('Fasth_Hospital1.s3db')
+        registro=conexion.cursor()
+        usuario=(idDoctor,Cedula,Foto,Nombre,Apellido,Tipo_Sangre,Email,Sexo,Fecha_Nacimiento,Alergias,Zodiaco)
+        sql='''INSERT INTO Pacientes(idDoctor,Cedula,Foto,Nombre,Apellido,Tipo_Sangre,Email,Sexo,Fecha_Nacimiento,Alergias,Zodiaco)VALUES(?,?,?,?,?,?,?,?,?,?,?)'''
+        registro.execute(sql,usuario)
+        conexion.commit()
+        return {"respuesta":"Los datos fueros registrados exitosamente"}
+    except TypeError:
+        return {"respuesta":"No se pudieron registrar los datos"}
+
+
+#Actualizar paciente................
+
+@app.get("/api/ActualizarPaciente/{idPaciente}/{Cedula}/{Foto}/{Nombre}/{Apellido}/{Tipo_Sangre}/{Email}/{Sexo}/{Fecha_Nacimiento}/{Alergias}/{Zodiaco}")
+def ActualizarPaciente(idPaciente:str,Cedula:str,Foto:str,Nombre:str,Apellido:str,Tipo_Sangre:str,Email:str,Sexo:str,Fecha_Nacimiento:str,Alergias:str,Zodiaco:str):
+    try:
+        conexion=sqlite3.connect('Fasth_Hospital1.s3db')
+        registro=conexion.cursor()
+        sql= "UPDATE Pacientes SET Cedula='"+Cedula+"',Foto='"+Foto+"',Nombre='"+Nombre+"',Apellido='"+Apellido+"',Tipo_Sangre='"+Tipo_Sangre+"',Email='"+Email+"',Sexo='"+Sexo+"',Fecha_Nacimiento='"+Fecha_Nacimiento+"',Alergias='"+Alergias+"',Zodiaco='"+Zodiaco+"' WHERE idPaciente="+idPaciente+""
+        registro.execute(sql)
+        conexion.commit()
+        return {"respuesta":"Los datos fueros actualizados exitosamente"}
+    except TypeError:
+        return {"respuesta":"No se pudieron actualizados los datos"}
+
+#Modificacion del Doctor
+
+@app.get("/api/modificar/{nombre}/{correo}/{token}")
+def modificar(nombre: str, correo: str, token: str):
+    try:
+        conexion = sqlite3.connect('Fasth_Hospital1.s3db')
+        cursor = conexion.cursor()
+        update = "UPDATE Doctores SET Nombre = '"+nombre+"', Email = '"+correo+"' WHERE idDoctor = '"+token+"'"
+        cursor.execute(update)
+        conexion.commit()
+        return {"respuesta":"Se modificaron los datos"}
+    except TypeError:
+        return {"respuesta":"No se pudieron modificar los datos"}
+
+#Modificacion de las credenciales del doctor
+
+@app.get("/api/ModClave/{clave_old}/{token}/{nuw_clave}")
+def modClave(new_clave: str, clave_old: str, token: str):
+    try:
+        conexion = sqlite3.connect('Fasth_Hospital1.s3db')
+        cursor = conexion.cursor()
+        updat = "UPDATE Doctores SET Clave ='"+new_clave+"' WHERE Clave = '"+clave_old+"' and idDoctor ='"+token+"'"
+        cursor.execute(updat)
+        conexion.commit()
+        return {"respuesta":"Se modificaron los datos"}
+    except TypeError:
+        return {"respuesta":"No se pudieron modificar los datos"}
